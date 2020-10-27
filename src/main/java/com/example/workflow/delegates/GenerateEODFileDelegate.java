161ca4779e324eb4356c4c8a92fde5c4f79e23b5
lucascalsilva <lucasc.alm.silva@gmail.com>
2020-10-27 17:16:22 +0100

    Added sample delegate that queries historical instances and variables

---
+++ src/main/java/com/example/workflow/delegates/GenerateEODFileDelegate.java
@@ -0,0 +1,37 @@
+package com.example.workflow.delegates;
+
+import lombok.RequiredArgsConstructor;
+import org.camunda.bpm.engine.HistoryService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.history.HistoricProcessInstance;
+import org.camunda.bpm.engine.history.HistoricVariableInstance;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class GenerateEODFileDelegate implements JavaDelegate {
+
+    private final HistoryService historyService;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        Date from = Date.from(LocalDate.now().atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+        List<HistoricProcessInstance> historicProcessInstances = historyService.createHistoricProcessInstanceQuery()
+                .executedActivityBefore(from)
+                .variableValueEquals("variable1", "value").list();
+
+        for(HistoricProcessInstance historicProcessInstance : historicProcessInstances){
+            List<HistoricVariableInstance> variableInstances = historyService.createHistoricVariableInstanceQuery()
+                    .processInstanceId(historicProcessInstance.getId()).list();
+
+            //Do something with the variables and with the historic process instance...
+        }
+    }
+}
